f6bc5b7844fecdb3c857960181acc6bf733e628b
Norp1x <grzegorz_dudek91@o2.pl>
2023-02-07 16:12:10 +0100

    Practice

--- src/Encapsulation.java
+++ src/Encapsulation.java
@@ -3,31 +3,39 @@ import java.util.Scanner;
 public class Encapsulation {
     public static void main(String[] args) {
         BankAccount savings = new BankAccount();
-        Scanner amount = new Scanner(System.in);
-        savings.setBalance(10000);
-        System.out.println("Account balance: " + savings.getBalance());
-        System.out.print("How much you want to withdraw?: ");
-        double withdraw = amount.nextDouble();
+        Scanner action = new Scanner(System.in);
 
-        if (savings.withdraw(withdraw)) {
-            System.out.println("Paid: " + withdraw);
-        }
-        else System.out.println("Not enough money");
-        System.out.print("Account balance: ");
-        System.out.println(savings.getBalance());
 
-        System.out.print("How much you want to deposit?: ");
-        double deposit = amount.nextDouble();
-        savings.deposit(deposit);
-        System.out.println("Deposit: ");
-        System.out.println(savings.getBalance());
+        do {
+            System.out.println("Account balance: " + savings.getBalance());
+            System.out.println("What do you want to do?");
+            System.out.println("1) Withdraw money");
+            System.out.println("2) Deposit money");
+            System.out.println("3) Exit");
+            int decision = action.nextInt();
+            switch (decision){
+                case 1 -> BankAccount.withdraw();
+                case 2 -> BankAccount.deposit();
+                case 3 -> {return;}
+            }
+//            if (decision == 1) {
+//                BankAccount.withdraw();
+//            } else if (decision == 2) {
+//                BankAccount.deposit();
+//            } else if (decision == 3){
+//                break;
+//            } else {
+//                System.out.println("Wrong action. Try one more time");
+//                return;
+//            }
+        } while (action.nextDouble() != 3);
     }
 }
 class BankAccount {
-    public BankAccount() {
-        balance = 1000;
-    }
     private double balance;
+    public BankAccount() {
+        balance = 10000;
+    }
     double getBalance() {
         return balance;
     }
@@ -45,5 +53,32 @@ class BankAccount {
     }
     void deposit(double amount) {
         setBalance(balance + amount);
+    }
+
+    // TODO: 07.02.2023 fix 'no continue' problem & saving amount to memory
+    public static void deposit() {
+        BankAccount savings = new BankAccount();
+        Scanner amount = new Scanner(System.in);
+        System.out.print("How much you want to deposit?: ");
+        double deposit = amount.nextDouble();
+        savings.deposit(deposit);
+        System.out.println("Deposit: " + deposit);
+        savings.setBalance(savings.getBalance());
+        System.out.println("Account balance: ");
+        System.out.println(savings.getBalance());
+
+    }
+    public static void withdraw(){
+        BankAccount savings = new BankAccount();
+        Scanner amount = new Scanner(System.in);
+        System.out.print("How much you want to withdraw?: ");
+        double withdraw = amount.nextDouble();
+
+        if (savings.withdraw(withdraw)) {
+            System.out.println("Paid: " + withdraw);
+        }
+        else System.out.println("Not enough money");
+        System.out.print("Account balance: ");
+        System.out.println(savings.getBalance());
     }
 }
